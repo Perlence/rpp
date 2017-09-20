26060cb0b12a2c91f6a1246514d4d5f313317448
Sviatoslav Abakumov <abakumov.sviatoslav@progforce.com>
2017-09-20 11:32:47 +0300

    Add classifiers and long description to setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
 from setuptools import setup
 
+with open('README.rst') as fp:
+    README = fp.read()
+
 setup(
     name='rpp',
     version='0.1',
     description='REAPER Project File Parser',
+    long_description=README,
     author='Sviatoslav Abakumov',
     author_email='dust.harvesting@gmail.com',
     license='BSD',
     url='https://github.com/Perlence/rpp',
+    download_url='https://github.com/Perlence/rpp/archive/master.zip',
     packages=['rpp'],
+    zip_safe=False,
     install_requires=[
         'ply',
         'attrs',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Text Processing',
     ])
