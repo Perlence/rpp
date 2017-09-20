4bf3a08759ea80bbb0f15f7b4bec3f9ea734c4d1
Sviatoslav Abakumov <abakumov.sviatoslav@progforce.com>
2017-09-20 11:07:13 +0300

    Move load and dump functions to a separate module

--- rpp/__init__.py
+++ rpp/__init__.py
@@ -1,29 +1,9 @@
 """RPP is a format used to describe REAPER <http://reaper.fm> projects."""
 
-from . import scanner
-from .decoder import parser
 from .element import Element
-from .encoder import encode
+from .rpp import dump, dumps, loads, load
 
 
 __version__ = '0.1'
 __author__ = 'Sviatoslav Abakumov <dust.harvesting@gmail.com>'
 __all__ = ['dump', 'dumps', 'load', 'loads', 'Element']
-
-
-def loads(string):
-    lexer = scanner.lexer()
-    yacc = parser()
-    return yacc.parse(string.strip(), lexer)
-
-
-def load(fp):
-    return loads(fp.read())
-
-
-def dumps(lists, indent=2):
-    return encode(lists, indent=indent)
-
-
-def dump(lists, fp, indent=2):
-    fp.write(dumps(lists, indent))
