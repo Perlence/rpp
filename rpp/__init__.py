e2292c4faee562dc3007cc16a1f547cc216cf111
Sviatoslav Abakumov <abakumov.sviatoslav@progforce.com>
2017-09-21 11:16:52 +0300

    Bump version to 0.2

--- rpp/__init__.py
+++ rpp/__init__.py
@@ -4,6 +4,6 @@ from .element import Element
 from .rpp import dump, dumps, loads, load
 
 
-__version__ = '0.1'
+__version__ = '0.2'
 __author__ = 'Sviatoslav Abakumov <dust.harvesting@gmail.com>'
 __all__ = ['dump', 'dumps', 'load', 'loads', 'Element']
