22e4dcc06e1e77127b585e80bd1b083fdaede074
Leigh Smith <leigh@leighsmith.com>
2017-12-06 14:01:40 -0500

    Bumped version number

--- rpp/__init__.py
+++ rpp/__init__.py
@@ -4,6 +4,6 @@ projects."""
 from .element import Element
 from .rpp import dump, dumps, loads, load
 
-__version__ = '0.3'
+__version__ = '0.4'
 __author__ = 'Sviatoslav Abakumov <dust.harvesting@gmail.com>'
 __all__ = ['dump', 'dumps', 'load', 'loads', 'Element']
