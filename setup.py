a53222364f51469572fd9b008d0d24fe23cca957
Sviatoslav Abakumov <dust.harvesting@gmail.com>
2017-12-07 00:13:55 +0300

    The package is not Python 3 only anymore

--- setup.py
+++ setup.py
@@ -31,6 +31,5 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3 :: Only',
         'Topic :: Text Processing',
     ])
