334cd2e1385e4653f0dfa1336b25f13f2469053f
Sviatoslav Abakumov <dust.harvesting@gmail.com>
2020-10-30 21:20:17 +0300

    Move metadata from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,37 +1,10 @@
 from setuptools import setup
 
-with open('README.rst') as fp:
-    README = fp.read()
-
 setup(
-    name='rpp',
-    version='0.4',
-    description='REAPER Project File Parser',
-    long_description=README,
-    author='Sviatoslav Abakumov',
-    author_email='dust.harvesting@gmail.com',
-    license='BSD',
-    url='https://github.com/Perlence/rpp',
-    download_url='https://github.com/Perlence/rpp/archive/0.4.tar.gz',
-    packages=['rpp'],
     zip_safe=False,
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     install_requires=[
         'ply',
         'attrs',
-    ],
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Topic :: Text Processing',
     ])
