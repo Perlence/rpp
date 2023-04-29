c5f05252c11a0e16983969e3a10a26565a9ded38
Sviatoslav Abakumov <dust.harvesting@gmail.com>
2023-04-29 22:33:33 +0400

    Bump version to 0.5

--- rpp/__init__.py
+++ rpp/__init__.py
@@ -5,6 +5,6 @@ from .element import Element
 from .rpp import dump, dumps, loads, load
 
 
-__version__ = '0.4'
+__version__ = '0.5'
 __author__ = 'Sviatoslav Abakumov <dust.harvesting@gmail.com>'
 __all__ = ['dump', 'dumps', 'load', 'loads', 'Element']
